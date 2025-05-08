378d3f80f6a7f2f3858001ce92dc44e585b29f5f
nabillahumi <nabillahumi003@gmail.com>
2025-05-08 12:41:14 +0700

    Semester 2 Jobsheet 10

---
+++ Mahasiswa18.java
@@ -0,0 +1,18 @@
+public class Mahasiswa18 {
+
+    String nim;
+    String nama;
+    String prodi;
+    String kelas;
+    
+    public Mahasiswa18(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilData() {
+        System.out.println(nim + " " + nama + " " + prodi + " " + kelas);
+    }
+}
